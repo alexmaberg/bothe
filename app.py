998db5443eafc45b586a8de0575d1230b83c8cd6
alexmaberg <49276291+alexmaberg@users.noreply.github.com>
2019-11-26 11:39:10 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+TOKEN: 1045281247:AAFRvLoa2buRnDCs8XDaFsQniQtXrWvzRJA
+APP_FILE: wsgi.py
+OPENSHIFT_REPO_DIR: [base dir of the project], i.e. /opt/app-root/src
+OPENSHIFT_GEAR_DNS: [domain of the app], see the hostname of the route
+OPENSHIFT_PYTHON_IP: 0.0.0.0
+OPENSHIFT_PYTHON_PORT: 8080
